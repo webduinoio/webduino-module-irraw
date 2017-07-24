1f04dc204d705c650ce45c2dcfc6e0b31606b456
osk2 <sc60714@gmail.com>
2017-07-24 11:53:28 +0800

    Update translation

--- blockly/msg/blocks/en.js
+++ blockly/msg/blocks/en.js
@@ -2,5 +2,5 @@ Blockly.Msg.WEBDUINO_IRRAW_SEND = 'IR send, pin:';
 Blockly.Msg.WEBDUINO_IRRAW_RECV = 'IR receiver, pin:';
 Blockly.Msg.WEBDUINO_IRRAW_LAUNCHCODE = 'Launch codes (m-16):';
 Blockly.Msg.WEBDUINO_IRRAW_ON = 'start receiving RAW IR';
-Blockly.Msg.WEBDUINO_IRRAW_CODE = 'receive code';
+Blockly.Msg.WEBDUINO_IRRAW_CODE = 'received code';
 Blockly.Msg.WEBDUINO_IRRAW_DO = 'do:';
