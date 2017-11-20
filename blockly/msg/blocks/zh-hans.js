9391c73da2c4abbaf259d961cb98b8e2a8771878
osk2 <sc60714@gmail.com>
2017-11-20 16:27:11 +0800

    Update String

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,6 @@
-Blockly.Msg.WEBDUINO_IRRAW_SEND = '红外线发射，脚位';
-Blockly.Msg.WEBDUINO_IRRAW_RECV = '红外线接收，脚位';
+Blockly.Msg.WEBDUINO_IRRAW_SEND = '冷气红外线发射，脚位';
+Blockly.Msg.WEBDUINO_IRRAW_RECV = '冷气红外线接收，脚位';
 Blockly.Msg.WEBDUINO_IRRAW_LAUNCHCODE = '发射代码 ( 十六进位 )';
-Blockly.Msg.WEBDUINO_IRRAW_ON = '开始侧录红外线';
+Blockly.Msg.WEBDUINO_IRRAW_ON = '开始接收冷气红外线';
 Blockly.Msg.WEBDUINO_IRRAW_CODE = '接收的代码';
 Blockly.Msg.WEBDUINO_IRRAW_DO = '执行';
